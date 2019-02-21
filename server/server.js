460ea22af01b0f61e7411b998cfb091a701df197
Vaibhav Mittal <vaibhav.mittal@ericsson.com>
2019-02-22 00:29:58 +0530

    corrected routing

--- server/server.js
+++ server/server.js
@@ -6,6 +6,9 @@ const publicPath = path.join(__dirname,'..','dist');
 
 app.use(express.static(publicPath));
 
+app.get('*',(req,res)=>{
+   res.sendFile(path.join(publicPath,'index.html'));
+});
 
 app.listen(8081,()=>{
     console.log('server is up!');
